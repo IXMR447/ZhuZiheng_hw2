6c3243e97792d585a4b2a3d3e69773af3112cc99
IXMR447 <04075257zzh@gmail.com>
2025-05-10 19:32:51 +0800

    finish

--- arduino_1/src/main.cpp
+++ arduino_1/src/main.cpp
@@ -3,19 +3,19 @@
 
   void setup() {
     // 设定主角（程序里的变量对应硬件的引脚），引脚功能/作用（输入/输出）
-  for(int i = 2; i < 6; i++)
+  for(int i = 2; i < 8; i++)
   pinMode(i,OUTPUT);
   }
    
   void loop() {
     //来回for循环，设置好起止点
-  for(int i = 2; i < 6; i++ )
+  for(int i = 2; i < 8; i++ )
     {
       digitalWrite(i , HIGH);
       delay(50);
       digitalWrite(i , LOW);
     }
-    for(int i = 5;i > 1;i--)
+    for(int i = 7;i > 1;i--)
     {
       digitalWrite(i , HIGH);
       delay(50);
